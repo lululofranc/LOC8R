0e1a030bc04dd4ae4033ae315f5de2483ac11de5
Lucian Cojocaru <lucian.cojocaru@lofranc.local>
2019-12-28 10:05:45 +0200

    234

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,13 +1,13 @@
 var testPage_Controller = function (req, res, err) {
     res.render('index', {
-        title: 'Test Title Heroku with Github 23 with function'
+        title: 'Test Title Heroku with Github 234 with function'
     }) // index => ..app_server/views/index.jade = template 
 };
 
 
 var homePage_Controller = function (req, res, err) {
     res.render('index', {
-        title: "My Controled on Github 23 Heroku - Home Page "
+        title: "My Controled on Github 234 Heroku - Home Page "
     })
 };
 
