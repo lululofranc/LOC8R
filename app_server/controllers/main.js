2c5b95723e218f2d73f2ce2a39ede1cce6366c8c
Lucian Cojocaru <lucian.cojocaru@lofranc.local>
2019-12-28 09:02:05 +0200

    change home page & test page

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,11 +1,17 @@
+var testPage_Controller = function (req, res, err) {
+    res.render('index', {
+        title: 'Test Title Heroku with function'
+    }) // index => ..app_server/views/index.jade = template 
+};
 
-var testPage_Controller = function(req,res,err) {
-    res.render('index',{title: 'Test Title with function'}) // index => ..app_server/views/index.jade = template 
-  };
-  
-  
-  var homePage_Controller = function(req,res,err) {
-    res.render('index',{title: "My Controled Home Page "})
-  };
 
- module.exports = {testPage_Controller,homePage_Controller}; 
+var homePage_Controller = function (req, res, err) {
+    res.render('index', {
+        title: "My Controled on Heroku - Home Page "
+    })
+};
+
+module.exports = {
+    testPage_Controller,
+    homePage_Controller
+};
